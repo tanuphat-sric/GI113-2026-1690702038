4ade768aff837958ac2e37f47fa2f18fc9e61bd7
tanuphat-sric <tanuphat.sric@bumail.net>
2026-08-25 10:39:28 +0700

    Add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :1690702038
+ * Name       :Tanuphat
+ * Section    :129C
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
