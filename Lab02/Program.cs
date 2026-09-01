7521560ec1e8f76f86564ee230b22302d2376d9f
tanuphat-sric <tanuphat.sric@bumail.net>
2026-09-01 21:23:14 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -63,21 +63,57 @@ namespace Lab02
             Console.WriteLine($"Crit rate: {critRate}");
             Console.WriteLine($"Is monster: {isMonster}");
 
-            Console.WriteLine();
+            string Name01 = "jane";
+            char Stage01 = 'C';
+            int mastery0 = 9;
+            int MAXHp01 = 250;
+            int currentHP01 = 150;
+            float ATK01 = 43.5f;
+            double critRate01 = 2.25;
+            bool isMonster01 = true;
 
-            hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP Percent: {hpPercent}%");
-            Console.WriteLine();
-            Console.WriteLine("Animan takes 60 damage!");
-            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {Name01}");
+            Console.WriteLine($"Stage: {Stage01}");
+            Console.WriteLine($"HP: {MAXHp01} / {currentHP01}");
+            Console.WriteLine($"ATK: {ATK01}");
+            Console.WriteLine($"Crit rate: {critRate01}");
+            Console.WriteLine($"Is monster: {isMonster01}");
 
-            currentHp = currentHp - 60;
-            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
-            Console.WriteLine($"HP: {currentHp} / {maxHp}");
-            hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP Percent: {hpPercent}%");
+            string Name02 = "Kai";
+            char Stage02 = 'S';
+            int mastery02 = 40;
+            int MAXHp02 = 400;
+            int currentHP02 = 320;
+            float ATK02 = 52.4f;
+            double critRate02 = 3.45;
+            bool isMonster02 = true;
 
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {Name02}");
+            Console.WriteLine($"Stage: {Stage02}");
+            Console.WriteLine($"HP: {MAXHp02} / {currentHP02}");
+            Console.WriteLine($"ATK: {ATK02}");
+            Console.WriteLine($"Crit rate: {critRate02}");
+            Console.WriteLine($"Is monster: {isMonster02}");
 
-        }
+            string Name03 = "Kai";
+            char Stage03 = 'S';
+            int mastery03 = 40;
+            int MAXHp03 = 400;
+            int currentHP03 = 320;
+            float ATK03 = 52.4f;
+            double critRate03 = 3.45;
+            bool isMonster03  = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {Name03}");
+            Console.WriteLine($"Stage: {Stage03}");
+            Console.WriteLine($"HP: {MAXHp03} / {currentHP03}");
+            Console.WriteLine($"ATK: {ATK03}");
+            Console.WriteLine($"Crit rate: {critRate03}");
+            Console.WriteLine($"Is monster: {isMonster03}");
+        }   
+       
     }
 }
