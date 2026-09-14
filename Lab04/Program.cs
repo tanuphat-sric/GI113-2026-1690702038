dad9a6e9167bbf0568005b9834959fe668accad5
tanuphat-sric <tanuphat.sric@bumail.net>
2026-09-15 00:30:33 +0700

    Revert "Lab04"
    This reverts commit 355377685ffc3833c72eaf1bd53198020ed53de5.

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -47,7 +47,7 @@ namespace Lab04
             bool classOk = int.TryParse(Console.ReadLine(), out int classNum);
             Console.Write("Starting luck (0.0-10.0): ");
             bool luckOk = double.TryParse(Console.ReadLine(), out double luck);
-            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luck}");
+            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luck}"); */
 
             Console.WriteLine("+------------------------------+");
             Console.WriteLine("|      CHARACTER CREATION       |");
@@ -85,36 +85,8 @@ namespace Lab04
             bool slotOk = int.TryParse(Console.ReadLine(), out int slot);
             Console.WriteLine($"Save name: {saveName}");
             Console.WriteLine($"Valid input: {slotOk}");
-            Console.WriteLine($"Slot: {slot}");*/
+            Console.WriteLine($"Slot: {slot}");
 
-            const string GameTitle = "ADVENTURE OF SKIBIDI";
-
-            var DummyName = "Cameraman";
-            var Rank = 'B';
-            int Level = 7;
-            float DMGcrit = 0.67f;
-            double speed = 37.5;
-            bool isPlayable = true;
-
-            Console.WriteLine($"<<<<<<<<<<< {GameTitle} >>>>>>>>>>>>");
-            Console.WriteLine($"<>Hero Name   : {DummyName}");
-            Console.WriteLine($"<>Rank        : {Rank}");
-            Console.WriteLine($"<>Level       : {Level}");
-            Console.WriteLine($"<>Crit Chance : {DMGcrit}");
-            Console.WriteLine($"<>Speed       : {speed}");
-            Console.WriteLine($"<>Playable    : {isPlayable}");
-
-            Console.WriteLine($"-<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>-");
-
-            double DummyLevelAsDouble = Level;
-            Console.WriteLine($"<:>Level as double (implicit): {DummyLevelAsDouble}");
-
-            int staminaTruncated = (int)speed;
-            int staminaRounded = Convert.ToInt32(speed);
-            Console.WriteLine($"<:>Speed cast   : {staminaTruncated}");
-            Console.WriteLine($"<:>Speed Convert   : {staminaRounded}");
-
-            Console.WriteLine($" ____   ___  ____    ____   ___  ____   __   _______ ______   _______ ____  _ \r\n|  _ \\ / _ \\|  _ \\  |  _ \\ / _ \\|  _ \\  \\ \\ / / ____/ ___\\ \\ / / ____/ ___|| |\r\n| | | | | | | |_) | | | | | | | | |_) |  \\ V /|  _| \\___ \\\\ V /|  _| \\___ \\| |\r\n| |_| | |_| |  __/  | |_| | |_| |  __/    | | | |___ ___) || | | |___ ___) |_|\r\n|____/ \\___/|_|     |____/ \\___/|_|       |_| |_____|____/ |_| |_____|____/(_)");
 
         }
     }
